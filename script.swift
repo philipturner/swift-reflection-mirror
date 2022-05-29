ff4d50205c0004ef81d125b51ce4eabc1c82d65f
Philip Turner <philipturner.AR@gmail.com>
2022-05-29 09:37:54 -0400

    Update script.swift

--- script.swift
+++ script.swift
@@ -133,9 +133,9 @@ extension AnyKeyPath {
     initializedBy body: (UnsafeMutableRawBufferPointer) -> Void
   ) -> Self {
     precondition(bytes > 0 && bytes % 4 == 0,
-     "capacity must be multiple of 4 bytes")
-    let result = Builtin.allocWithTailElems_1(
-      self, (bytes/4)._builtinWordValue, Int32.self)
+                 "capacity must be multiple of 4 bytes")
+    let result = Builtin.allocWithTailElems_1(self, (bytes/4)._builtinWordValue,
+                                              Int32.self)
     
     // Workaround for the fact that `_kvcKeyPathStringPtr` is API-internal.
     // Emulates the code: `result._kvcKeyPathStringPtr = nil`
@@ -146,13 +146,214 @@ extension AnyKeyPath {
       print("Original pointer:", opaque)
     }
     
-    let base = UnsafeMutableRawPointer(
-      Builtin.projectTailElems(result, Int32.self))
+    let base = UnsafeMutableRawPointer(Builtin.projectTailElems(result,
+                                                                Int32.self))
     let bufferPointer = UnsafeMutableRawBufferPointer(start: base, count: bytes)
     print("Second pointer:", bufferPointer)
     body(bufferPointer)
     print("Finished _create")
     return result
+  }
+}
+
+// MARK: Implementation details
+internal enum KeyPathComponentKind {
+  /// The keypath references an externally-defined property or subscript whose
+  /// component describes how to interact with the key path.
+  case external
+  /// The keypath projects within the storage of the outer value, like a
+  /// stored property in a struct.
+  case `struct`
+  /// The keypath projects from the referenced pointer, like a
+  /// stored property in a class.
+  case `class`
+  /// The keypath projects using a getter/setter pair.
+  case computed
+  /// The keypath optional-chains, returning nil immediately if the input is
+  /// nil, or else proceeding by projecting the value inside.
+  case optionalChain
+  /// The keypath optional-forces, trapping if the input is
+  /// nil, or else proceeding by projecting the value inside.
+  case optionalForce
+  /// The keypath wraps a value in an optional.
+  case optionalWrap
+}
+
+internal struct RawKeyPathComponent {
+  internal var header: Header
+  internal var body: UnsafeRawBufferPointer
+
+  internal init(header: Header, body: UnsafeRawBufferPointer) {
+    self.header = header
+    self.body = body
+  }
+  
+  internal struct Header {
+    internal var _value: UInt32
+    
+    init(discriminator: UInt32, payload: UInt32) {
+      _value = 0
+      self.discriminator = discriminator
+      self.payload = payload
+    }
+    
+    internal var discriminator: UInt32 {
+      get {
+        return (_value & Header.discriminatorMask) >> Header.discriminatorShift
+      }
+      set {
+        let shifted = newValue << Header.discriminatorShift
+        precondition(shifted & Header.discriminatorMask == shifted,
+                     "discriminator doesn't fit")
+        _value = _value & ~Header.discriminatorMask | shifted
+      }
+    }
+    internal var payload: UInt32 {
+      get {
+        return _value & Header.payloadMask
+      }
+      set {
+        precondition(newValue & Header.payloadMask == newValue,
+                     "payload too big")
+        _value = _value & ~Header.payloadMask | newValue
+      }
+    }
+    internal var endOfReferencePrefix: Bool {
+      get {
+        return _value & Header.endOfReferencePrefixFlag != 0
+      }
+      set {
+        if newValue {
+          _value |= Header.endOfReferencePrefixFlag
+        } else {
+          _value &= ~Header.endOfReferencePrefixFlag
+        }
+      }
+    }
+    
+    internal var kind: KeyPathComponentKind {
+      switch (discriminator, payload) {
+      case (Header.externalTag, _):
+        return .external
+      case (Header.structTag, _):
+        return .struct
+      case (Header.classTag, _):
+        return .class
+      case (Header.computedTag, _):
+        return .computed
+      case (Header.optionalTag, Header.optionalChainPayload):
+        return .optionalChain
+      case (Header.optionalTag, Header.optionalWrapPayload):
+        return .optionalWrap
+      case (Header.optionalTag, Header.optionalForcePayload):
+        return .optionalForce
+      default:
+        preconditionFailure("invalid header")
+      }
+    }
+    
+    internal static var payloadMask: UInt32 {
+      return _SwiftKeyPathComponentHeader_PayloadMask
+    }
+    internal static var discriminatorMask: UInt32 {
+      return _SwiftKeyPathComponentHeader_DiscriminatorMask
+    }
+    internal static var discriminatorShift: UInt32 {
+      return _SwiftKeyPathComponentHeader_DiscriminatorShift
+    }
+    internal static var externalTag: UInt32 {
+      return _SwiftKeyPathComponentHeader_ExternalTag
+    }
+    internal static var structTag: UInt32 {
+      return _SwiftKeyPathComponentHeader_StructTag
+    }
+    internal static var computedTag: UInt32 {
+      return _SwiftKeyPathComponentHeader_ComputedTag
+    }
+    internal static var classTag: UInt32 {
+      return _SwiftKeyPathComponentHeader_ClassTag
+    }
+    internal static var optionalTag: UInt32 {
+      return _SwiftKeyPathComponentHeader_OptionalTag
+    }
+    internal static var optionalChainPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_OptionalChainPayload
+    }
+    internal static var optionalWrapPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_OptionalWrapPayload
+    }
+    internal static var optionalForcePayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_OptionalForcePayload
+    }
+    
+    internal static var endOfReferencePrefixFlag: UInt32 {
+      return _SwiftKeyPathComponentHeader_EndOfReferencePrefixFlag
+    }
+    internal static var storedMutableFlag: UInt32 {
+      return _SwiftKeyPathComponentHeader_StoredMutableFlag
+    }
+    internal static var storedOffsetPayloadMask: UInt32 {
+      return _SwiftKeyPathComponentHeader_StoredOffsetPayloadMask
+    }
+    internal static var outOfLineOffsetPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_OutOfLineOffsetPayload
+    }
+    internal static var unresolvedFieldOffsetPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_UnresolvedFieldOffsetPayload
+    }
+    internal static var unresolvedIndirectOffsetPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_UnresolvedIndirectOffsetPayload
+    }
+    internal static var maximumOffsetPayload: UInt32 {
+      return _SwiftKeyPathComponentHeader_MaximumOffsetPayload
+    }
+    
+    // The component header is 4 bytes, but may be followed by an aligned
+    // pointer field for some kinds of component, forcing padding.
+    internal static var pointerAlignmentSkew: Int {
+      return MemoryLayout<Int>.size - MemoryLayout<Int32>.size
+    }
+    
+    init(stored kind: KeyPathStructOrClass,
+         mutable: Bool,
+         inlineOffset: UInt32) {
+      let discriminator: UInt32
+      switch kind {
+      case .struct: discriminator = Header.structTag
+      case .class: discriminator = Header.classTag
+      }
+
+      precondition(inlineOffset <= Header.maximumOffsetPayload)
+      let payload = inlineOffset
+        | (mutable ? Header.storedMutableFlag : 0)
+      self.init(discriminator: discriminator,
+                payload: payload)
+    }
+  }
+  
+  internal func clone(into buffer: inout UnsafeMutableRawBufferPointer,
+             endOfReferencePrefix: Bool) {
+    var newHeader = header
+    newHeader.endOfReferencePrefix = endOfReferencePrefix
+    
+    var componentSize = MemoryLayout<Header>.size
+    buffer.storeBytes(of: newHeader, as: Header.self)
+    switch header.kind {
+    case .struct,
+         .class:
+      break // add more code
+    case .optionalChain,
+         .optionalForce,
+         .optionalWrap:
+      break
+    case .computed:
+      break // add more code
+    case .external:
+      preconditionFailure("should have been instantiated away")
+    }
+    buffer = UnsafeMutableRawBufferPointer(
+      start: buffer.baseAddress.unsafelyUnwrapped + componentSize,
+      count: buffer.count - componentSize)
   }
 }
 
@@ -162,17 +363,64 @@ internal struct KeyPathBuffer {
     internal init(_ buffer: UnsafeMutableRawBufferPointer) {
       self.buffer = buffer
     }
+    internal mutating func pushRaw(
+      size: Int, alignment: Int
+    ) -> UnsafeMutableRawBufferPointer {
+      var baseAddress = buffer.baseAddress.unsafelyUnwrapped
+      var misalign = Int(bitPattern: baseAddress) & (alignment - 1)
+      if misalign != 0 {
+        misalign = alignment - misalign
+        baseAddress = baseAddress.advanced(by: misalign)
+      }
+      let result = UnsafeMutableRawBufferPointer(
+        start: baseAddress,
+        count: size)
+      buffer = UnsafeMutableRawBufferPointer(
+        start: baseAddress + size,
+        count: buffer.count - size - misalign)
+      return result
+    }
+    internal mutating func push<T>(_ value: T) {
+      let buf = pushRaw(size: MemoryLayout<T>.size,
+                        alignment: MemoryLayout<T>.alignment)
+      buf.storeBytes(of: value, as: T.self)
+    }
+    internal mutating func pushHeader(_ header: Header) {
+      push(header)
+      // Start the components at pointer alignment
+      _ = pushRaw(size: RawKeyPathComponent.Header.pointerAlignmentSkew,
+             alignment: 4)
+      print("Finished pushHeader")
+    }
+  }
+  
+  internal struct Header {
+    internal var _value: UInt32
+    internal init(size: Int, trivial: Bool, hasReferencePrefix: Bool) {
+      precondition(size <= Int(Header.sizeMask),
+                   "key path too big")
+      _value = UInt32(size)
+        | (trivial ? Header.trivialFlag : 0)
+        | (hasReferencePrefix ? Header.hasReferencePrefixFlag : 0)
+    }
+    
+    internal static var sizeMask: UInt32 {
+      return _SwiftKeyPathBufferHeader_SizeMask
+    }
+    internal static var reservedMask: UInt32 {
+      return _SwiftKeyPathBufferHeader_ReservedMask
+    }
+    internal static var trivialFlag: UInt32 {
+      return _SwiftKeyPathBufferHeader_TrivialFlag
+    }
+    internal static var hasReferencePrefixFlag: UInt32 {
+      return _SwiftKeyPathBufferHeader_HasReferencePrefixFlag
+    }
   }
 }
 
-internal struct RawKeyPathComponent {
-  internal struct Header {
-    // The component header is 4 bytes, but may be followed by an aligned
-    // pointer field for some kinds of component, forcing padding.
-    internal static var pointerAlignmentSkew: Int {
-      return MemoryLayout<Int>.size - MemoryLayout<Int32>.size
-    }
-  }
+internal enum KeyPathStructOrClass {
+  case `struct`, `class`
 }
 
 func my_forEachField() -> Bool {
@@ -209,9 +457,22 @@ func my_forEachField() -> Bool {
     }
     let resultSize = MemoryLayout<Int32>.size + MemoryLayout<Int>.size
     let partialKeyPathType = _openExistential(childType, do: keyPathType)
-      ._create(capacityInBytes: resultSize, initializedBy: { _ in
-        
-      })
+       ._create(capacityInBytes: resultSize) {
+      var destBuilder = KeyPathBuffer.Builder($0)
+      destBuilder.pushHeader(KeyPathBuffer.Header(
+        size: resultSize - MemoryLayout<Int>.size,
+        trivial: true,
+        hasReferencePrefix: false
+      ))
+      let component = RawKeyPathComponent(
+           header: RawKeyPathComponent.Header(stored: .struct,
+                                              mutable: field.isVar,
+                                              inlineOffset: UInt32(offset)),
+           body: UnsafeRawBufferPointer(start: nil, count: 0))
+      component.clone(
+        into: &destBuilder.buffer,
+        endOfReferencePrefix: false)
+    }
     
     
     
