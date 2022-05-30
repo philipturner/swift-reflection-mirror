f287d31eca95971a9957db146050e83118d6c736
Philip Turner <philipturner.AR@gmail.com>
2022-05-29 20:15:54 -0400

    Update ReflectionMirror.swift

--- Sources/ReflectionMirror/ReflectionMirror.swift
+++ Sources/ReflectionMirror/ReflectionMirror.swift
@@ -1,9 +1,14 @@
+//===----------------------------------------------------------------------===//
 //
-//  ReflectionMirror.swift
+// This source file is part of the Swift.org open source project
 //
+// Copyright (c) 2014 - 2020 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
 //
-//  Created by Philip Turner on 5/29/22.
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
 //
+//===----------------------------------------------------------------------===//
 
 import SwiftShims
 
